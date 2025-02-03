c710482ca4d5097ae7f22636f9ced142b3f123da
lucaslnasc <lucaslnascimento015@gmail.com>
2025-02-03 19:29:40 -0300

    docs: repositório de estudos e aperfeiçoamentos no framework nestJS

---
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
+import { z } from 'zod'
+import { QuestionPresenter } from '../presenters/question-presenter'
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema)
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>
+
+@Controller('/questions')
+export class FetchRecentQuestionsController {
+  constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
+  ) {
+    const result = await this.fetchRecentQuestions.execute({
+      page,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const questions = result.value.questions
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
+  }
+}
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import swc from 'unplugin-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  test: {
+    globals: true,
+    root: './',
+  },
+});
